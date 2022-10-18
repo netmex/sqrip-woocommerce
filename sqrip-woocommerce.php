b0ba580ceae34e5a809a5a17416c062cbf1d9f40
MMM <info@netmex.ch>
2022-10-18 16:49:15 +0200

    1.5.5
    Changed Plugin Name to sqrip.ch

--- sqrip-woocommerce.php
+++ sqrip-woocommerce.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * Plugin Name:             sqrip – Swiss QR Invoice
+ * Plugin Name:             sqrip.ch
  * Plugin URI:              https://sqrip.ch/
  * Description:             sqrip – A comprehensive and clever WooCommerce finance tool for the most widely used payment method in Switzerland: the bank transfers. 
- * Version:                 1.5.6
+ * Version:                 1.5.5
  * Author:                  netmex digital gmbh
  * Author URI:              https://sqrip.ch/
  */
